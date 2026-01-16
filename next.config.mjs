bd039d249de87f21762c69ea653c9482d3ba5274
abomansur <abomansur@gmail.com>
2026-01-16 22:37:47 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // هذا السطر يحول الموقع لملفات HTML ثابتة
-  images: {
-    unoptimized: true, // ضروري جداً لظهور الصور على هوستنجر
-  },
+  // أزلنا إعدادات التصدير لأن فيرسل ذكي وسيقوم بكل شيء تلقائياً
 };
 
 export default nextConfig;
